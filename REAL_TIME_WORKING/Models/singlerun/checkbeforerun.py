33d85cbd69db2614a12c8ba8774ccd9aaf0cc500
Adwait Kulkarni <151489169+WalnutEagle@users.noreply.github.com>
2024-10-04 02:09:10 -0400

    Delete REAL_TIME_WORKING directory

--- REAL_TIME_WORKING/Models/singlerun/checkbeforerun.py
+++
@@ -1,7 +0,0 @@
-from missing import check_dataset, find_missing_files
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Imitation Learning DataSet Checking Script')
-    parser.add_argument('-d', '--data_folder', default="/home/h2x/Desktop/IL_DATA_COLLECTION_ADWAIT/Main_script/09-15-2024", type=str, help='Path to your dataset')
-    args = parser.parse_args()
-    find_missing_files(args.data_folder)
-    check_dataset(args.data_folder)
