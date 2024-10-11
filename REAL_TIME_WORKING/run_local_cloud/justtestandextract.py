c8faeaff2e30f671e3dc51789a8a7ff13026bfa4
Adwait Kulkarni <adk1361@bu.edu>
2024-10-11 01:59:21 -0400

    Update justtestandextract.py

--- REAL_TIME_WORKING/run_local_cloud/justtestandextract.py
+++ REAL_TIME_WORKING/run_local_cloud/justtestandextract.py
@@ -24,8 +24,11 @@ from server import start_server, receive_data, send_response
 model_path = "/home/h2x/Desktop/UniLCD_RealWorld/REAL_TIME_WORKING/run_local_cloud/model_run_0011.pth"
 full_path = "/home/h2x/Desktop/REAL_TIME_WORKING/Main_script/10-04-2024/rc_data/run_001"
 if __name__== "__main__":
+    
     conn = start_server()
+    t1 = time.time()
     output = get_preds(model_path, full_path)
     send_response(conn, output)
     serveroutput = receive_data(conn)
+    print(f"Total Time taken is {time.time(0-t1)}seconds.")
     print(f"Yay the commms worked {serveroutput}")
