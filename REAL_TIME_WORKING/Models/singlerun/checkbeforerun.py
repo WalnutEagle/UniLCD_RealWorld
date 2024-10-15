93a09956a9eefa5aabf032f5bd60213e10ab9a96
Adwait Kulkarni <adk1361@bu.edu>
2024-10-15 14:41:57 -0400

    Update checkbeforerun.py

--- REAL_TIME_WORKING/Models/singlerun/checkbeforerun.py
+++ REAL_TIME_WORKING/Models/singlerun/checkbeforerun.py
@@ -1,4 +1,8 @@
 from missing import check_dataset, find_missing_files
+import os
+import glob
+import json
+import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Imitation Learning DataSet Checking Script')
     parser.add_argument('-d', '--data_folder', default="/home/h2x/Desktop/IL_DATA_COLLECTION_ADWAIT/Main_script/09-15-2024", type=str, help='Path to your dataset')
