bcb82cf932c1d33784ccebe43a733ccd5cef11b9
Adwait Kulkarni <adk1361@bu.edu>
2024-10-16 11:41:47 -0400

    Update get_power.py

--- REAL_TIME_WORKING/power_stats/get_power.py
+++ REAL_TIME_WORKING/power_stats/get_power.py
@@ -2,20 +2,13 @@ from jtop import jtop
 
 def get_jetson_stats():
     with jtop() as jetson:
-        cpu_usage = jetson.cpu
-        gpu_usage = jetson.gpu
-        power_usage = jetson.power
 
-        print(f"CPU Usage: {cpu_usage}%")
-        print(f"GPU Usage: {gpu_usage}%")
-        print("Power Usage Data:", power_usage)
+        power_usage = jetson.power
         if 'tot' in power_usage and 'power' in power_usage['tot']:
             total_power = power_usage['tot']['power'] / 1000 
             print(f"Power Usage: {total_power:.2f} Watts")
         else:
             print("Power usage data not available.")
-
-        print("-" * 40)  
 
 if __name__ == "__main__":
     try:
