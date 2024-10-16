2c04be7648f9d9ddf62d4e299696878d29f9df85
Adwait Kulkarni <adk1361@bu.edu>
2024-10-16 10:45:49 -0400

    Update run_local.py

--- REAL_TIME_WORKING/run_local/run_local.py
+++ REAL_TIME_WORKING/run_local/run_local.py
@@ -33,6 +33,7 @@ from PIL import Image
 from collections import deque
 
 
+
 i2c_bus0 = busio.I2C(board.SCL, board.SDA)
 kit = ServoKit(channels=16, i2c=i2c_bus0, address=0x40)
 kit.servo[0].set_pulse_width_range(1200, 2200)
@@ -304,8 +305,8 @@ def main():
                     output = get_preds(model_path, full_path)
                     print(f"Total Time: {time.time() - s:.5f}")
                     # print(output)
-                    if output[0][1]>=0.95:
-                        output[0][1] = 1.0
+                    # if output[0][1]>=0.95:
+                    #     output[0][1] = 1.0
                     # print(f"Steering:{output[0][0]}, Throttle:{output[0][1]}")
                     # throttle = output[0][1]
                     # steer = output[0][0]
