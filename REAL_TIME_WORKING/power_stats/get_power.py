eaa3fc61bb8dd9e5966219bbfc28a8492f6b9e76
Adwait Kulkarni <adk1361@bu.edu>
2024-10-16 11:32:48 -0400

    Update get_power.py

--- REAL_TIME_WORKING/power_stats/get_power.py
+++ REAL_TIME_WORKING/power_stats/get_power.py
@@ -4,12 +4,10 @@ def get_jetson_stats():
     with jtop() as jetson:
         cpu_usage = jetson.cpu
         gpu_usage = jetson.gpu
-        memory_usage = jetson.memory
         power_usage = jetson.power
 
         print(f"CPU Usage: {cpu_usage}%")
         print(f"GPU Usage: {gpu_usage}%")
-        print(f"Memory Usage: {memory_usage['used'] / memory_usage['total'] * 100:.2f}%")
         print(f"Power Usage: {power_usage['power'] / 1_000_000:.2f} Watts")
         print("-" * 40)
 
