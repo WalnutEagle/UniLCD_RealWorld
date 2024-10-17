a959986454d32a1fe702182eec57af1d7891510e
Adwait Kulkarni <adk1361@bu.edu>
2024-10-17 10:29:11 -0400

    Update run_local.py

--- REAL_TIME_WORKING/run_local/run_local.py
+++ REAL_TIME_WORKING/run_local/run_local.py
@@ -318,6 +318,8 @@ def main():
                     else :
                         mapped_steer = map_value_steer(output[0][0])
                         mapped_throttle = map_value_throttle(output[0][1])
+                    if mapped_throttle > 99.0:
+                        mapped_throttle = 99.0
                     # print(f"steer {mapped_steer}, throttle {mapped_throttle}")
                     kit.servo[0].angle = mapped_steer
                     kit.servo[1].angle = mapped_throttle
