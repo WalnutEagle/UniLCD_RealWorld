f82bdd018c3516affadd4925c970128248a99e58
Adwait Kulkarni <adk1361@bu.edu>
2024-10-23 13:35:07 -0400

    Update runthis.py

--- REAL_TIME_WORKING/runthis.py
+++ REAL_TIME_WORKING/runthis.py
@@ -1,4 +1,4 @@
-from REAL_TIME_WORKING.power_stats.get_power import get_jetson_stats
+from power_stats.get_power import get_jetson_stats
 if __name__ == '__main__':
     while True:
         well = get_jetson_stats()
