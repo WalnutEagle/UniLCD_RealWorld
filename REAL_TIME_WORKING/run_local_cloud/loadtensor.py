a968bf3f33978a4ae26da73c4bffef9ac308b6ef
Adwait Kulkarni <adk1361@bu.edu>
2024-10-25 14:55:41 -0400

    Update loadtensor.py

--- REAL_TIME_WORKING/run_local_cloud/loadtensor.py
+++ REAL_TIME_WORKING/run_local_cloud/loadtensor.py
@@ -5,4 +5,5 @@ import numpy as np
 tensor_path = '/opt/app-root/src/UniLCD_RealWorld/output_tensor.pt'  # Update with your actual path
 t1 = time.time()
 loaded_tensor = torch.load(tensor_path)
+print(loaded_tensor)
 print((time.time()-t1)*1000)
