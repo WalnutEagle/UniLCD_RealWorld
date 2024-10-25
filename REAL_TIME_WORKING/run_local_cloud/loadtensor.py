b3d1ace8d6597fb67bf62807a1068110e45c0a8d
Adwait Kulkarni <adk1361@bu.edu>
2024-10-25 14:54:22 -0400

    Create loadtensor.py

---
+++ REAL_TIME_WORKING/run_local_cloud/loadtensor.py
@@ -0,0 +1,8 @@
+import logging
+import torch
+import time
+import numpy as np
+tensor_path = '/opt/app-root/src/UniLCD_RealWorld/output_tensor.pt'  # Update with your actual path
+t1 = time.time()
+loaded_tensor = torch.load(tensor_path)
+print((time.time()-t1)*1000)
