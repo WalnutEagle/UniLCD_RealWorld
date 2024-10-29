1c4e7479a6970717f45a3dbab25a1a252efb54c8
Adwait Kulkarni <adk1361@bu.edu>
2024-10-29 19:11:59 -0400

    Update orin.py

--- REAL_TIME_WORKING/new_inference/newcomms/orin.py
+++ REAL_TIME_WORKING/new_inference/newcomms/orin.py
@@ -3,7 +3,7 @@ import torch
 
 # Start the server
 server_2_soc = start_server()  # Renamed from 'socket' to 'server_2_soc'
-data = torch.rand(1, 4, 150, 130)  # Sample tensor data
+data = torch.rand(1, 4)  # Sample tensor data
 received_data, addr = receive_data(server_2_soc)
 print(addr)
 try:
