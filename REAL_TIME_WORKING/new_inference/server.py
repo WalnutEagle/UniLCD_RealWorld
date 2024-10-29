a1038cb6439536785f7652c3c1d4f6e813e1c732
Adwait Kulkarni <adk1361@bu.edu>
2024-10-29 01:42:20 -0400

    Update server.py

--- REAL_TIME_WORKING/new_inference/server.py
+++ REAL_TIME_WORKING/new_inference/server.py
@@ -39,23 +39,23 @@ def send_response(server_socket, response, addr):
         server_socket.sendto(data[i:i + chunk_size], addr)  # Send each chunk
 
 # Main server loop function for processing data
-# def server_loop(server_socket):
-#     while True:
-#         received_data, addr = receive_data(server_socket)      
-#         # Handle text or tensor data
-#         if isinstance(received_data, str):
-#             print(f"Received text message: {received_data} from {addr}")
-#             send_response(server_socket, "Text received!", addr)
-#         elif isinstance(received_data, torch.Tensor):
-#             print(f"Received PyTorch tensor data: \n{received_data} from {addr}")
-#             print(f"It took{(time.time()-s)*1000} Miliseconds.")
-#             tensor_data = torch.rand(1, 4, 150, 130)
-#             t1 = time.time()
-#             send_response(server_socket, tensor_data, addr)
-#             print(f"Tensor Sent, {(time.time()-t1)*1000} Miliseconds")
-#         else:
-#             print(f"Received unknown data type: {type(received_data)} from {addr}")
-#             send_response(server_socket, "Unknown data type received!", addr)
+def server_loop(server_socket):
+    while True:
+        received_data, addr = receive_data(server_socket)      
+        # Handle text or tensor data
+        if isinstance(received_data, str):
+            print(f"Received text message: {received_data} from {addr}")
+            send_response(server_socket, "Text received!", addr)
+        elif isinstance(received_data, torch.Tensor):
+            print(f"Received PyTorch tensor data: \n{received_data} from {addr}")
+            print(f"It took{(time.time()-s)*1000} Miliseconds.")
+            tensor_data = torch.rand(1, 4, 150, 130)
+            t1 = time.time()
+            send_response(server_socket, tensor_data, addr)
+            print(f"Tensor Sent, {(time.time()-t1)*1000} Miliseconds")
+        else:
+            print(f"Received unknown data type: {type(received_data)} from {addr}")
+            send_response(server_socket, "Unknown data type received!", addr)
 
 # # Usage example:
 # server_socket = start_server()
