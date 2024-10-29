a71e67e545b1cbdab778a6d7a20d245452d94b0a
Adwait Kulkarni <adk1361@bu.edu>
2024-10-29 02:02:18 -0400

    lll

--- REAL_TIME_WORKING/communication/udp/client.py
+++ REAL_TIME_WORKING/communication/udp/client.py
@@ -32,31 +32,31 @@ def receive_response(client_socket):
     return pickle.loads(data)
 
 # Main client communication loop (can be called repeatedly)
-# def client_loop(client_socket):
-#     while True:
-#         choice = input("Enter 't' for text, 'n' for tensor, 'q' to quit: ")
+def client_loop(client_socket):
+    while True:
+        choice = input("Enter 't' for text, 'n' for tensor, 'q' to quit: ")
         
-#         if choice == 't':
-#             text_message = input("Enter your text message: ")
-#             send_data(client_socket, text_message)
-#             print("Server response:", receive_response(client_socket))
+        if choice == 't':
+            text_message = input("Enter your text message: ")
+            send_data(client_socket, text_message)
+            print("Server response:", receive_response(client_socket))
         
-#         elif choice == 'n':
-#             tensor_data = torch.rand(2, 2)  # Example PyTorch tensor data
-#             start = time.time()
-#             print(f"Sending PyTorch tensor: \n{tensor_data}")
-#             send_data(client_socket, tensor_data)
-#             print(f"Data sent in {(time.time()-start)*1000} Miliseconds")
-#             t1 = time.time()
-#             print("Server response:", receive_response(client_socket))
-#             print(f"Recived data in:{(time.time()-t1)*1000}Miliseconds.")
+        elif choice == 'n':
+            tensor_data = torch.rand(2, 2)  # Example PyTorch tensor data
+            start = time.time()
+            print(f"Sending PyTorch tensor: \n{tensor_data}")
+            send_data(client_socket, tensor_data)
+            print(f"Data sent in {(time.time()-start)*1000} Miliseconds")
+            t1 = time.time()
+            print("Server response:", receive_response(client_socket))
+            print(f"Recived data in:{(time.time()-t1)*1000}Miliseconds.")
             
-#         elif choice == 'q':
-#             print("Closing connection...")
-#             break
+        elif choice == 'q':
+            print("Closing connection...")
+            break
 
-#     client_socket.close()
+    client_socket.close()
 
-# # Usage example:
-# client_socket = connect_to_server()
-# client_loop(client_socket)
+# Usage example:
+client_socket = connect_to_server()
+client_loop(client_socket)
