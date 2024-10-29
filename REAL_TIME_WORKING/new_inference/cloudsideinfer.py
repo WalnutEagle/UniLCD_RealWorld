afd22257c0545471f90b6f4c60b7c68ae73a15e2
Adwait Kulkarni <adk1361@bu.edu>
2024-10-29 12:02:41 -0400

    Update cloudsideinfer.py

--- REAL_TIME_WORKING/new_inference/cloudsideinfer.py
+++ REAL_TIME_WORKING/new_inference/cloudsideinfer.py
@@ -43,8 +43,9 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
     print(device)
-    client_sock = connect_to_server()
-    client_loop(client_sock)
+    while True:
+        client_sock = connect_to_server()
+        client_loop(client_sock)
     # inferr(device, client_sock)
     # client_sock.close()
     # client_loop(client_sock)
