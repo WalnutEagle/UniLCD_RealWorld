a96e63d858c3e6ba999a618223cb34240e7e4565
Adwait Kulkarni <adk1361@bu.edu>
2024-10-28 22:24:31 -0400

    Update merger.py

--- REAL_TIME_WORKING/Models/ovrft/merger.py
+++ REAL_TIME_WORKING/Models/ovrft/merger.py
@@ -2,7 +2,7 @@ import logging
 import torch
 import numpy as np
 from torch.utils.data import DataLoader
-from cloud1_dataloader import CarlaDataset  # Ensure this is correct
+from Models.ovrft.cloud1_dataloader import CarlaDataset  # Ensure this is correct
 from cloud1_model import CustomRegNetY002  # Ensure this matches your model definition
 
 def load_model(model_path):
