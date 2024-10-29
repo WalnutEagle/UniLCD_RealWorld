0b5d2f8075258a730bc2d4a9f43d923d3521ec64
Adwait Kulkarni <adk1361@bu.edu>
2024-10-29 13:00:10 -0400

    Update cloudsideinfer.py

--- REAL_TIME_WORKING/new_inference/cloudsideinfer.py
+++ REAL_TIME_WORKING/new_inference/cloudsideinfer.py
@@ -44,9 +44,9 @@ if __name__ == '__main__':
     model.to(device)
     print(device)
     client_sock = connect_to_server()
-    while True:
-        time.sleep(1)
-        client_loop(client_sock)
+    # while True:
+    time.sleep(1)
+    client_loop(client_sock)
     # inferr(device, client_sock)
     # client_sock.close()
     # client_loop(client_sock)
