f291784ca3c3247f5c5cc922352033d24506cda2
Adwait Kulkarni <adk1361@bu.edu>
2024-10-29 12:26:18 -0400

    as

--- REAL_TIME_WORKING/new_inference/client_udp.py
+++ REAL_TIME_WORKING/new_inference/client_udp.py
@@ -33,32 +33,32 @@ def receive_response(client_socket):
 
 # Main client communication loop (can be called repeatedly)
 def client_loop(client_socket):
-    while True:
-        time.sleep(1.2)
-        # choice = input("Enter 't' for text, 'n' for tensor, 'q' to quit: ")
-        choice = 'n'
+    # while True:
+    time.sleep(0.12)
+    # choice = input("Enter 't' for text, 'n' for tensor, 'q' to quit: ")
+    choice = 'n'
+    
+    if choice == 't':
+        text_message = input("Enter your text message: ")
+        send_data(client_socket, text_message)
+        print("Server response:", receive_response(client_socket))
+    
+    elif choice == 'n':
+        tensor_data = torch.rand(2, 2)  # Example PyTorch tensor data
+        start = time.time()
+        print(f"Sending PyTorch tensor: \n{tensor_data}")
+        send_data(client_socket, tensor_data)
+        print(f"Data sent in {(time.time()-start)*1000} Miliseconds")
+        t1 = time.time()
+        print("Server response:", receive_response(client_socket))
+        print(f"Recived data in:{(time.time()-t1)*1000}Miliseconds.")
         
-        if choice == 't':
-            text_message = input("Enter your text message: ")
-            send_data(client_socket, text_message)
-            print("Server response:", receive_response(client_socket))
-        
-        elif choice == 'n':
-            tensor_data = torch.rand(2, 2)  # Example PyTorch tensor data
-            start = time.time()
-            print(f"Sending PyTorch tensor: \n{tensor_data}")
-            send_data(client_socket, tensor_data)
-            print(f"Data sent in {(time.time()-start)*1000} Miliseconds")
-            t1 = time.time()
-            print("Server response:", receive_response(client_socket))
-            print(f"Recived data in:{(time.time()-t1)*1000}Miliseconds.")
-            
-        elif choice == 'q':
-            print("Closing connection...")
-            break
+    elif choice == 'q':
+        print("Closing connection...")
+            # break
 
-    client_socket.close()
+    # client_socket.close()
 
 # Usage example:
-client_socket = connect_to_server()
-client_loop(client_socket)
+# client_socket = connect_to_server()
+# client_loop(client_socket)
