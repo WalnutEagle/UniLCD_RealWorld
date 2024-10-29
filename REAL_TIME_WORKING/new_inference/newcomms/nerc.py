8d42193638e7d8e97092401a5845f7a53930b0df
Adwait Kulkarni <adk1361@bu.edu>
2024-10-29 18:58:27 -0400

    :hash:

--- REAL_TIME_WORKING/new_inference/newcomms/nerc.py
+++ REAL_TIME_WORKING/new_inference/newcomms/nerc.py
@@ -1,1 +1,11 @@
 from newclient import receive_response, send_data, connect_to_server
+import torch
+
+socket = connect_to_server()
+send_data(socket, '', timeout=5)
+while True:
+    tensord = torch.rand(2, 2)
+    send_data(socket, tensord)
+    response = receive_response(socket)
+    if KeyboardInterrupt:
+        break
--- REAL_TIME_WORKING/new_inference/newcomms/orin.py
+++ REAL_TIME_WORKING/new_inference/newcomms/orin.py
@@ -2,4 +2,10 @@ from newserver import send_response, receive_data, start_server
 import torch
 socket = start_server()
 data = torch.rand(1,4,150,130)
-send_response
+icom, addr = receive_data(socket)
+while True:
+    send_response(socket, data, addr)
+    res, addr = receive_data(socket)
+    print(res)
+    if KeyboardInterrupt:
+        break
